45777ed0a0661eeeb3e6833fa156b50670dc69d3
wql2002 <wqlthu2020@gmail.com>
2024-07-04 12:25:08 +0800

    Add scripts to bootstrap set up process

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Author: JJGO
+# source: https://github.com/JJGO/dotfiles/blob/master/setup_all.sh
+
+# Dont link DS_Store files
+find . -name ".DS_Store" -exec rm {} \;
+
+PROGRAMS=(bash env git tmux vim zsh)
+
+OLD_DOTFILES="dotfile_bak_$(date -u +"%Y%m%d%H%M%s")"
+
+mkdir ${OLD_DOTFILES}
+
+function backup_if_exists() {
+	if [ -f $1 ];
+	then
+	  mv $1 ${OLD_DOTFILES}
+	fi
+	if [ -d $1 ];
+	then
+	  mv $1 ${OLD_DOTFILES}
+	fi
+}
+
+# Clean common conflicts
+backup_if_exists ~/.bash_profile
+backup_if_exists ~/.bashrc
+backup_if_exists ~/.zshrc
+backup_if_exists ~/.gitconfig
+backup_if_exists ~/.tmux.conf
+backup_if_exists ~/.profile
+
+for program in ${PROGRAMS[@]}; do
+	stow -v --target=$HOME $program
+	echo "Configuring $program"
+done
+
+echo "Done!"
