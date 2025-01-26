a409acecb7b88a3281214eaea38edf385ce1cd8d
wql2002 <wqlthu2020@gmail.com>
2025-01-26 04:37:58 +0000

    fix(zsh): add checking

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,10 +4,16 @@
 if ! [ -x "$(command -v zsh)" ]; then
     echo "Installing zsh"
     sudo apt install zsh
+fi
 
+# check if oh-my-zsh exists
+if ! [ -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
+# check if powerlevel10k exists
+if ! [ -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
     echo "Installing powerlevel10k"
     git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 fi
